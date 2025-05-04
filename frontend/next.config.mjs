a1ff8c502fe663050751a45082f09a5b4fa9257f
TheAngelTech <tech@theangelservices.com>
2025-05-05 02:43:58 +0530

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,15 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async redirects() {
-      return [
-        {
-          source: '/home',
-          destination: '/',
-          permanent: true,
-        },
-      ];
-    },
-  };
-  
-  export default nextConfig;
-  
+  async redirects() {
+    return [
+      {
+        source: '/home',
+        destination: '/',
+        permanent: true,
+      },
+    ];
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+export default nextConfig;
