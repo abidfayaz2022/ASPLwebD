0e288dfb387066662de191e15beed99361ae9d14
TheAngelTech <tech@theangelservices.com>
2025-05-05 13:09:51 +0530

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,7 +6,7 @@ cd /home/ubuntu/angel/frontend
 npm ci
 npm run build
 
-Next.js on 0.0.0.0:80 (root required)
+# Next.js on 0.0.0.0:80 (root required)
 nohup npx next start --hostname 0.0.0.0 --port 80 \
   > /var/log/frontend-next.log 2>&1 &
 
