07265835df6f1226a8341221367ab6d3f5f1a553
Abid Fayaz <bhataabid4411@gmail.com>
2025-05-22 02:21:08 +0530

    fixed

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -13,7 +13,7 @@ export const refreshToken = async (req, res) => {
     res.cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+     sameSite: 'None',
       maxAge: 24 * 60 * 60 * 1000 // 1 day
     });
 
@@ -49,7 +49,7 @@ export const login = async (req, res) => {
     res.cookie('refreshToken', refreshToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      sameSite: 'None',
       maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
     });
 
@@ -57,7 +57,7 @@ export const login = async (req, res) => {
     res.cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: 'strict',
+      sameSite: 'None',
       maxAge: 24 * 60 * 60 * 1000 // 1 day
     });
 
@@ -78,13 +78,13 @@ export const logout = (_req, res) => {
   res.clearCookie('token', {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
+    sameSite: 'None',
   });
 
   res.clearCookie('refreshToken', {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
+    sameSite: 'None',
   });
 
   res.status(200).json({ success: true, message: 'Logged out successfully' });
