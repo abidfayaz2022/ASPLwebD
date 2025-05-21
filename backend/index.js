6c0aea5b800c56d6125f6aa0e94c9bd46c0198e8
Abid Fayaz <bhataabid4411@gmail.com>
2025-05-22 01:46:13 +0530

    production error

--- backend/index.js
+++ backend/index.js
@@ -76,5 +76,5 @@ app.use((err, req, res, _next) => {
 
 // Start Server
 app.listen(PORT, '0.0.0.0', () => {
-  console.log(`✅ Server running at http://0.0.0.0:${PORT}`);
+  console.log(` Server running at http://0.0.0.0:${PORT}`);
 });
